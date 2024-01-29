1ec8f0ee9cb6ab7a035c8f0f0398d52ec28aa48a
Nickgaming8889 <nickgaming8889@gmail.com>
2024-01-29 11:32:51 -0600

    29/01/2024 11:32

--- Practice 4/Practica4_Factorial_Nicolas_Tadeo_Caceres_Panuco.cpp
+++ Practice 4/Practica4_Factorial_Nicolas_Tadeo_Caceres_Panuco.cpp
@@ -17,6 +17,10 @@ int fact(int *pa_){
     int *rlt = (int*)malloc(sizeof(int));    
     int j = 1;
 
+    if (*pa_ == 0) {
+        return *rlt = 1;
+    }
+
     for (int i = 1; i <= *pa_; i++) {
         *rlt = j *= i;
     }
--- Practice 5/Practice5_Fibonacci_Nicolas_Tadeo_Caceres_Panuco.cpp
+++ Practice 5/Practice5_Fibonacci_Nicolas_Tadeo_Caceres_Panuco.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 
+int p1 = 0, p2 = 1;
+
 int fibonacci(int *);
 
 int main(int argc, char const *argv[]){
     int a, *pa;
 
-        std::cout << "Give me a number: ";
-        std:: cin >> a;
-        pa = &a;
+    std::cout << "\nGive me a number: ";
+    std:: cin >> a;
+    pa = &a;
 
-        std::cout << fibonacci(pa);
+    fibonacci(pa);
     
     return 0;
 }
 
 int fibonacci(int *pa_) {
     int *rlt = (int*)malloc(sizeof(int));
-    int fibonacci;
-
+    int fib;
     
+    if (*pa_ == 0) {
+        std::cout << "Invalid input, try again" << std::endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= *pa_; i++) {
+        if (i > 2) {
+            fib = p1 + p2;
+            p1 = p2;
+            p2 = fib;
+            std::cout << fib;
+        }
+        else if (i == 2) {
+            std::cout << p2;
+        }
+        else if (i == 1) {
+            std::cout << p1;
+        }
+    }
 
     return 0;
 }

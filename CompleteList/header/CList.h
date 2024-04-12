2e47bf715c55ecbec2c9f7177afb0477e0b370f7
Nickgaming8889 <nickgaming8889@gmail.com>
2024-04-11 21:41:00 -0600

    2024.04.11 21:40

--- CompleteList/header/CList.h
+++ CompleteList/header/CList.h
@@ -1,3 +1,49 @@
 #ifndef CLIST_H
 #define CLIST_H
 
+#include "iostream"
+using namespace std;
+
+class User {
+    private:
+        //Attributes...
+        string name;
+        string email;
+        int id;
+    public:
+        //Methods...
+        User();
+        ~User();
+
+        //Getters & Setters...
+        string getName();
+        void setName(string);
+        string getEmail();
+        void setEmail(string);
+        int getID();
+        void setID(int);
+};
+
+class Node : public User {
+    private:
+        //Attributes...
+        User *user;
+        Node *front;
+        Node *rear;
+
+    public:
+        //Methods...
+};
+
+class CList : public Node {
+    private:
+        //Attributes...
+        Node *head;
+
+    public:
+        //Methods...
+        CList();
+        ~CList();
+};
+
+#endif
